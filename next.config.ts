5abf21499f7b9bca1a9bf9f93487dfe05cdd4649
akashbiswas0 <akash8860biswas@gmail.com>
2025-06-13 23:55:35 +0530

    testing Tomo SDK

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  transpilePackages: [
+    '@tomo-inc/tomo-evm-kit',
+    '@tomo-wallet/uikit-lite',
+    '@tomo-inc/shared-type'
+  ],
 };
 
 export default nextConfig;
